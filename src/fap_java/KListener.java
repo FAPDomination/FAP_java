4d38471d50c8fa8d8f0340fa0e1d7d9f3e2a6995
Leonard <leonard.allain-launay@insa-lyon.fr>
2013-07-01 18:53:48 +0200

    Weee, multiple displacement implemented

--- src/fap_java/KListener.java
+++ src/fap_java/KListener.java
@@ -7,13 +7,16 @@ import java.util.ArrayList;
 public class KListener implements KeyListener{
     
     private ArrayList<Player> players;
+    private ArrayList<Integer> pressed = new ArrayList<Integer>();
     
     public KListener(Game game) {
         super();
         players = game.getPlayers();
     }
 
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+            
+    }
 
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
@@ -22,9 +25,14 @@ public class KListener implements KeyListener{
             Player p = players.get(i);
             int[][] keys = p.getKeys();
             for(int j=0; j<keys.length;j++){
-                if(code == keys[j][0]){
+                if(code == keys[j][0] && !pressed.contains(code)){
+                    pressed.add(code);
+                    //p.keyHigh(j);
+                   // break;
+                }
+                
+                if(pressed.contains(keys[j][0])){
                     p.keyHigh(j);
-                    break;
                 }
             }
         }
@@ -32,6 +40,9 @@ public class KListener implements KeyListener{
 
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
+        while(pressed.contains(code)){
+            pressed.remove((Object)code);
+        }
         
         for(int i = 0;i<players.size();i++){
             Player p = players.get(i);
@@ -42,5 +53,6 @@ public class KListener implements KeyListener{
                 }
             }
         }
+
     }
 }
