631f1896bbb45d9cdb3d390ad27ea9e99565011d
Leonard <leonard.allain-launay@insa-lyon.fr>
2013-08-10 13:25:11 +0200

    MC and javadoc/comment

--- src/fap_java/Tools.java
+++ src/fap_java/Tools.java
@@ -1,6 +1,12 @@
 package fap_java;
 
 public class Tools {
+    /**
+     * Picks a random int between two value
+     * @param min : min boundary
+     * @param max : max boundary
+     * @return : a random integer between min and max
+     */
     public static int randRange(int min, int max) {
         int randomNum = (int)(Math.random() * (max - min + 1) + min);
         return randomNum;
