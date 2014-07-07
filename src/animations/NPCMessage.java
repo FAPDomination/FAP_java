8ea9ba93ecd41fc5a763201249f1f3e4c19a302e
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2014-07-07 08:24:47 +0200

    NPC Message will only display if the position is set. To be tested

--- src/animations/NPCMessage.java
+++ src/animations/NPCMessage.java
@@ -30,8 +30,8 @@ public class NPCMessage extends Animation {
     private int height=77;
     private int margins = 10;
     
-    private int x;
-    private int y;
+    private int x=0;
+    private int y=0;
     public NPCMessage(String message, String yes, String no,TheComputingThread thread, AAsk launcher, boolean bAsk) {
         super(0,0,0,thread);
         ask = bAsk;
@@ -67,31 +67,30 @@ public class NPCMessage extends Animation {
 
     public void paintComponent(Graphics g) {
         g.setColor(Color.BLACK);
-
-        Graph.drawDarkBackgroundRectangle(g, x, y, width, height, Graph.NPC_SQUARE_COLOR, 20, Graph.BG_DARK);
-        //g.drawImage(img,x,y,width,height,thread.getMyGame());
-
-        //g.drawString(message, x+20, y+20);
-        if(ask){
-            int fac;
-            choice = launcher.isChoice();
-            if(choice){
-                fac=0;
+        if(x != 0 && y != 0){
+            Graph.drawDarkBackgroundRectangle(g, x, y, width, height, Graph.NPC_SQUARE_COLOR, 20, Graph.BG_DARK);
+            //g.drawImage(img,x,y,width,height,thread.getMyGame());
+    
+            //g.drawString(message, x+20, y+20);
+            if(ask){
+                int fac;
+                choice = launcher.isChoice();
+                if(choice){
+                    fac=0;
+                }
+                else{
+                    fac = 1;
+                }
+                
+                g.setColor(Color.WHITE);
+                //TODO positions
+                g.drawString(yes, x+(width/4), y+50);       // these positions are bad, to be re-made
+                g.drawString(no, x+(3*width/4), y+50);
+                //TODO paint cursor
+                g.fillRect(x+(width/4)-6+fac*2*width/4, y+45, 4,4);       // Whole cursor to be re-made
             }
-            else{
-                fac = 1;
-            }
-            
-            g.setColor(Color.WHITE);
-            //TODO positions
-            g.drawString(yes, x+(width/4), y+50);       // these positions are bad, to be re-made
-            g.drawString(no, x+(3*width/4), y+50);
-            //TODO paint cursor
-            g.fillRect(x+(width/4)-6+fac*2*width/4, y+45, 4,4);       // Whole cursor to be re-made
-        }
         
-        //TODO multiline
-        //TODO resizable sizes (incl. multiline) and positions
+        }
     }
 
     public void executeAnimation(){}
